36b2055bafce70a7d58aadac6aac4e618348a51b
barbudor <barbu@barbudor.net>
2021-11-21 11:44:51 +0100

    add log2 file

--- config/run_psmqtt.sh
+++ config/run_psmqtt.sh
@@ -2,4 +2,4 @@
 
 cd ~/psmqtt
 [[ -d venv ]] && source ./venv/bin/activate
-PSMQTTCONFIG=~/psmqtt/config/`hostname`.conf python psmqtt.py  &
+PSMQTTCONFIG=~/psmqtt/config/`hostname`.conf python psmqtt.py >>psmqtt2.log 2>&1 &
